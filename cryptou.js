7d7bed65de08da5e56186c5c513811458f226ebd
Ben <105461985+ben-ekw@users.noreply.github.com>
2023-05-24 17:07:06 -0700

    random number generator

--- cryptou.js
+++ cryptou.js
@@ -1,3 +1,25 @@
+/**
+ * cryptou | MIT license | https://github.com/Ekwani-Consulting/cryptou
+ *  UMD Crypto library (supports AMD, Node, CommonJS, modulejs, YUI, and browsers)
+ *
+ * Public Methods:
+ *
+ * version
+ * @returns {String} cryptou version
+ *
+ * random
+ * @param {Integer} size - in bytes; default = 32
+ * @returns {Uint8Array} random entropy in specified byte size
+ *
+ * ***********************************************************************************************
+ * Sources:
+ *
+ * ClientRandom                         | MIT license       | https://github.com/verifyprovablyfair/ClientRandom
+ * fid-umd                              | MIT license       | https://github.com/fidian/fid-umd
+ * UMD (Universal Module Definition)    | MIT license       | https://github.com/umdjs/umd
+ *
+ * ***********************************************************************************************
+ */
 (function (name, root, factory) {
   if (typeof define === "function" && define.amd) {
     // AMD
@@ -22,10 +44,58 @@
   }
 })("cryptou", typeof self !== "undefined" ? self : this, function () {
   "use strict";
-  const version = "0.1.0";
 
-  // Exposed public methods
+  function version() {
+    return "0.1.0";
+  }
+
+  /*
+   * ***********************************************************************************************
+   *
+   * Random Number Generator
+   *    forked from https://github.com/verifyprovablyfair/ClientRandom/blob/master/ClientRandom.js
+   *
+   * ***********************************************************************************************
+   */
+  function genrand() {
+    var rng_nums = new Uint32Array(2);
+    if (
+      typeof window === "object" &&
+      window.crypto &&
+      window.crypto.getRandomValues
+    )
+      window.crypto.getRandomValues(rng_nums); // Chrome, Opera, Firefox
+    else if (
+      typeof window === "object" &&
+      window.msCrypto &&
+      window.msCrypto.getRandomValues
+    )
+      window.msCrypto.getRandomValues(rng_nums); // IE
+    else
+      rng_nums = [
+        Math.floor(Math.random() * 4294967295),
+        Math.floor(Math.random() * 4294967295),
+      ]; // All outdated browsers
+    return (rng_nums[0] >>> 5) * 67108864.0 + (rng_nums[1] >>> 6);
+  }
+
+  function random(size = 32) {
+    const block = [];
+    for (let i = 0; i < size; i++) {
+      block.push(genrand());
+    }
+    return new Uint8Array(block);
+  }
+
+  /*
+   * ***********************************************************************************************
+   *
+   * Expose Public Methods
+   *
+   * ***********************************************************************************************
+   */
   return {
     version: version,
+    random: random,
   };
 });
